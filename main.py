9ce2c98d342ef3b54762847a4bb4c974c7751fde
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2015-07-28 09:30:42 +1200

    added scrolling

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ grid = [[0 for x in range(c.gridx_dim)] for y in range(c.gridy_dim)]
 pygame.init()
 
 size = c.window_size
+camera = [0, 0]
+mouse_pos = tuple(v//2 for v in size)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption(c.window_caption)
@@ -37,13 +39,27 @@ while not quitflag:
         if event.type == pygame.QUIT:
             quitflag = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            column = pos[0] // (c.WIDTH+ c.MARGIN)
-            row = pos[1] // (c.HEIGHT + c.MARGIN)
+            mouse_pos = pygame.mouse.get_pos()
+            column = (mouse_pos[0] + camera[0]) // (c.WIDTH+ c.MARGIN)
+            row = (mouse_pos[1] + camera[1]) // (c.HEIGHT + c.MARGIN)
             grid[row][column] = 0
             print("row: " + str(row) + " column: " + str(column))
+        elif event.type == pygame.MOUSEMOTION:
+            mouse_pos = pygame.mouse.get_pos()
 
     '''Game logic goes here'''
+    if mouse_pos[0] < 100:
+        camera[0] -= 4
+    elif mouse_pos[0] >= c.window_size[0]-100:
+        camera[0] += 4
+
+    if mouse_pos[1] < 100:
+        camera[1] -= 4
+    elif mouse_pos[1] >= c.window_size[1]-100:
+        camera[1] += 4
+
+    camera[0] = max(0, min((c.WIDTH+c.MARGIN)*c.gridx_dim - c.window_size[0], camera[0]))
+    camera[1] = max(0, min((c.HEIGHT+c.MARGIN)*c.gridy_dim - c.window_size[1], camera[1]))
 
     '''Rendering/Drawing goes here (Below the white screen fill)'''
 
@@ -55,8 +71,8 @@ while not quitflag:
             colour = c.WHITE
             pygame.draw.rect(screen,
                              colour,
-                             [(c.MARGIN + c.WIDTH) * column + c.MARGIN,
-                              (c.MARGIN + c.HEIGHT) * row + c.MARGIN,
+                             [(c.MARGIN + c.WIDTH) * column + c.MARGIN - camera[0],
+                              (c.MARGIN + c.HEIGHT) * row + c.MARGIN - camera[1],
                               c.WIDTH,
                               c.HEIGHT])
 
